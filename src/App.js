5e1adf25bb987fdf960a4e37dcd4b0a03c06a756
githubniggo <nico.zollinger@lernende.bbw.ch>
2022-06-29 10:29:39 +0200

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+  const [players, setPlayers] = useState([])
+  useEffect(() => {
+    fetch("http://localhost:8080/Spieler")
+        .then((response) => response.json())
+        .then((data) => setPlayers(data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div>Schweizermeister FCZ</div>
+
+      </>
   );
 }
 
